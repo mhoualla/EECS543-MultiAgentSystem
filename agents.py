0adc9891b9a1afeba728f56d8c8573d787ddf6f4
mhoualla <mhoualla@umich.edu>
2024-10-13 10:59:37 -0400

    update: more parameters and attributes to agents

--- agents.py
+++ agents.py
@@ -1,18 +1,20 @@
 class Agent:
-    def __init__(self, name, age, gender, race, income, position):
+    def __init__(self, name, age, gender, income, position, values, biases):
         self.name = name
         self.age = age
         self.gender = gender
-        self.race = race
         self.income = income
         self.position = position
+        self.values = values 
+        self.biases = biases
 
     def get_demographics(self):
         return {
             'name': self.name,
             'age': self.age,
             'gender': self.gender,
-            'race': self.race,
             'income': self.income,
-            'position': self.position
+            'position': self.position,
+            'values': self.values,
+            'biases': self.biases,
         }
